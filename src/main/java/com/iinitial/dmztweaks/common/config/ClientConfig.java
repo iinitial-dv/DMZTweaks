2f23c8c1b58611b7e7669f84bfb87f39026e5c02
iinitial <noah.robinsg@gmail.com>
2026-08-10 17:39:06 -0400

    feat: add fields, getters, and setters for client config

--- src/main/java/com/iinitial/dmztweaks/common/config/ClientConfig.java
+++ src/main/java/com/iinitial/dmztweaks/common/config/ClientConfig.java
@@ -1,5 +1,46 @@
 package com.iinitial.dmztweaks.common.config;
 
 public final class ClientConfig {
-    // TODO: Client config getters, setters, and fields
+    // Shows the actually calculated health/stamina/ki gain or drain instead of the base value.
+    public boolean enableHealthGain;
+    public boolean enableStaminaGain;
+    public boolean enableKiGain;
+    // Shows the same calculated health/stamina/ki gain or drain in the hud so players can see it there as well.
+    public boolean enableHealthGainInHud;
+    public boolean enableKiGainInHud;
+    public boolean enableStaminaGainInHud;
+
+    // Getters for Client related Configs
+    public boolean isHealthGainEnabled() { return enableHealthGain; }
+    public boolean isStaminaGainEnabled() { return enableStaminaGain; }
+    public boolean isKiGainEnabled() { return enableKiGain; }
+    public boolean isHealthGainInHudEnabled() { return enableHealthGainInHud; }
+    public boolean isKiGainInHudEnabled() { return enableKiGainInHud; }
+    public boolean isStaminaGainInHudEnabled() { return enableStaminaGainInHud; }
+
+    // Setters for Client related Configs
+    public void setHealthGain(boolean value) {
+        enableHealthGain = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setStaminaGain(boolean value) {
+        enableStaminaGain = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setKiGain(boolean value) {
+        enableKiGain = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setHealthGainInHud(boolean value) {
+        enableHealthGainInHud = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setKiGainInHud(boolean value) {
+        enableKiGainInHud = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setStaminaGainInHud(boolean value) {
+        enableStaminaGainInHud = value;
+        ConfigManager.saveClientConfig();
+    }
 }
