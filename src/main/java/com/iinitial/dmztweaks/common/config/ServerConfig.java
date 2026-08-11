eede37ae490d89fc50ac83c4b5bae74ef6577c21
iinitial <noah.robinsg@gmail.com>
2026-08-10 20:06:49 -0400

    style: renamed fields, getters, and setters

--- src/main/java/com/iinitial/dmztweaks/common/config/ServerConfig.java
+++ src/main/java/com/iinitial/dmztweaks/common/config/ServerConfig.java
@@ -4,39 +4,39 @@ public class ServerConfig {
     // Balance changes made to classes so that tank isn't the most picked class.
     public boolean enableBalancedClasses;
     // Arrows above enemies before they attack.
-    public boolean telegraphedAttacksEnabled;
+    public boolean enableTelegraphedAttacks;
     // Changes the calculated health/stamina/ki gain to be precise instead of rounding to the whole number when applying
     // to character. The displayed number will still be a whole number. The change is behind the scenes.
-    public boolean preciseHealthGainEnabled;
-    public boolean preciseStaminaGainEnabled;
-    public boolean preciseKiGainEnabled;
+    public boolean enablePreciseHealthGain;
+    public boolean enablePreciseStaminaGain;
+    public boolean enablePreciseKiGain;
 
     // Getters for Server related Configs
     public boolean isBalancedClassesEnabled() { return enableBalancedClasses; }
-    public boolean isTelegraphedAttacksEnabled() { return telegraphedAttacksEnabled; }
-    public boolean isPreciseHealthGainEnabled() { return preciseHealthGainEnabled; }
-    public boolean isPreciseStaminaGainEnabled() { return preciseStaminaGainEnabled; }
-    public boolean isPreciseKiGainEnabled() { return preciseKiGainEnabled; }
+    public boolean isTelegraphedAttacksEnabled() { return enableTelegraphedAttacks; }
+    public boolean isPreciseHealthGainEnabled() { return enablePreciseHealthGain; }
+    public boolean isPreciseStaminaGainEnabled() { return enablePreciseStaminaGain; }
+    public boolean isPreciseKiGainEnabled() { return enablePreciseKiGain; }
 
     // Setters for Server related Configs
-    public void setBalancedClassesEnabled(boolean value) {
+    public void setBalancedClasses(boolean value) {
         enableBalancedClasses = value;
         ConfigManager.saveServerConfig();
     }
-    public void setTelegraphedAttacksEnabled(boolean value) {
-        telegraphedAttacksEnabled = value;
+    public void setTelegraphedAttacks(boolean value) {
+        enableTelegraphedAttacks = value;
         ConfigManager.saveServerConfig();
     }
-    public void setPreciseHealthGainEnabled(boolean value) {
-        preciseHealthGainEnabled = value;
+    public void setPreciseHealthGain(boolean value) {
+        enablePreciseHealthGain = value;
         ConfigManager.saveServerConfig();
     }
-    public void setPreciseStaminaGainEnabled(boolean value) {
-        preciseStaminaGainEnabled = value;
+    public void setPreciseStaminaGain(boolean value) {
+        enablePreciseStaminaGain = value;
         ConfigManager.saveServerConfig();
     }
-    public void setPreciseKiGainEnabled(boolean value) {
-        preciseKiGainEnabled = value;
+    public void setPreciseKiGain(boolean value) {
+        enablePreciseKiGain = value;
         ConfigManager.saveServerConfig();
     }
 }
