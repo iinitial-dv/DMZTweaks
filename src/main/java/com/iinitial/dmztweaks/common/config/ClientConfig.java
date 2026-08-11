474cabfd11c13517cd6109aa7e1cd4a06f71ff71
iinitial <noah.robinsg@gmail.com>
2026-08-10 20:00:30 -0400

    feat: added new config for telegraphed attacks

--- src/main/java/com/iinitial/dmztweaks/common/config/ClientConfig.java
+++ src/main/java/com/iinitial/dmztweaks/common/config/ClientConfig.java
@@ -1,24 +1,32 @@
 package com.iinitial.dmztweaks.common.config;
 
 public final class ClientConfig {
+    // Shows the arrow above enemies when about to attack if the setting is enabled in server config
+    public boolean enableTelegraphedAttacks;
     // Shows the actually calculated health/stamina/ki gain or drain instead of the base value.
     public boolean enableHealthGain;
     public boolean enableStaminaGain;
     public boolean enableKiGain;
     // Shows the same calculated health/stamina/ki gain or drain in the hud so players can see it there as well.
     public boolean enableHealthGainInHud;
-    public boolean enableKiGainInHud;
     public boolean enableStaminaGainInHud;
+    public boolean enableKiGainInHud;
 
     // Getters for Client related Configs
+    public boolean isTelegraphedAttacksEnabled() { return enableTelegraphedAttacks; }
     public boolean isHealthGainEnabled() { return enableHealthGain; }
     public boolean isStaminaGainEnabled() { return enableStaminaGain; }
     public boolean isKiGainEnabled() { return enableKiGain; }
     public boolean isHealthGainInHudEnabled() { return enableHealthGainInHud; }
-    public boolean isKiGainInHudEnabled() { return enableKiGainInHud; }
     public boolean isStaminaGainInHudEnabled() { return enableStaminaGainInHud; }
+    public boolean isKiGainInHudEnabled() { return enableKiGainInHud; }
 
     // Setters for Client related Configs
+    public void setTelegraphedAttacks(boolean value) {
+        enableTelegraphedAttacks = value;
+        ConfigManager.saveClientConfig();
+    }
+
     public void setHealthGain(boolean value) {
         enableHealthGain = value;
         ConfigManager.saveClientConfig();
@@ -35,12 +43,12 @@ public final class ClientConfig {
         enableHealthGainInHud = value;
         ConfigManager.saveClientConfig();
     }
-    public void setKiGainInHud(boolean value) {
-        enableKiGainInHud = value;
-        ConfigManager.saveClientConfig();
-    }
     public void setStaminaGainInHud(boolean value) {
         enableStaminaGainInHud = value;
+        ConfigManager.saveClientConfig();
+    }
+    public void setKiGainInHud(boolean value) {
+        enableKiGainInHud = value;
         ConfigManager.saveClientConfig();
     }
 }
